07ed88e70822776f3be1054727077c90a3ff454d
BB Caspian <casfian@gmail.com>
2022-09-19 11:54:05 +0800

    added notification code

---
+++ lib/buatbadge.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+import 'package:awesome_notifications/awesome_notifications.dart';
+
+//change to stateful
+class BuatBadge extends StatefulWidget {
+  const BuatBadge({super.key});
+
+  @override
+  State<BuatBadge> createState() => _BuatBadgeState();
+}
+
+class _BuatBadgeState extends State<BuatBadge> {
+  @override
+  void initState() {
+    super.initState();
+
+    //step 2. pop-up pemission
+    AwesomeNotifications().isNotificationAllowed().then((isAllowed) {
+      if (!isAllowed) {
+        //alertbox
+        AlertDialog alert = AlertDialog(
+          title: const Text('Allow Notification'),
+          content: const Text('Do you Allow?'),
+          actions: [
+            TextButton(
+                onPressed: () => AwesomeNotifications()
+                    .requestPermissionToSendNotifications()
+                    .then((_) => Navigator.pop(context)),
+                child: const Text('Yes')),
+            TextButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: const Text('No')),
+          ],
+        );
+        showDialog(
+            context: context,
+            builder: (context) {
+              return alert;
+            });
+      }
+    });
+
+    //utk dengar Notification kita
+    AwesomeNotifications()
+        .actionStream
+        .listen((ReceivedNotification receivedNotification) {
+      //code
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Buat Badge'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+            onPressed: () async {
+              //code
+              await AwesomeNotifications().createNotification(
+                  content: NotificationContent(
+                      id: 19,
+                      channelKey: 'basic_channel',
+                      title: 'Simple Notification',
+                      body: 'Simple body',
+                      notificationLayout: NotificationLayout.Default,
+                      ));
+                      
+            },
+            child: const Text('Buat Badge')),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:tempahanbilikadmin/authenticate.dart';
 import 'package:tempahanbilikadmin/authentication.dart';
-import 'package:tempahanbilikadmin/home.dart';
+import 'package:tempahanbilikadmin/buatbadge.dart';
+import 'package:awesome_notifications/awesome_notifications.dart';
 
 //utk Authenticate
 import 'package:firebase_auth/firebase_auth.dart';
@@ -11,6 +11,31 @@ import 'package:provider/provider.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
+  
+  //step 1. Notification
+  AwesomeNotifications().initialize(
+      // set the icon to null if you want to use the default app icon
+      'resource://drawable/res_notification_app_icon',
+      [
+        NotificationChannel(
+            channelGroupKey: 'basic_channel_group',
+            channelKey: 'basic_channel',
+            channelName: 'Basic notifications',
+            channelDescription: 'Notification channel for basic tests',
+            defaultColor: const Color(0xFF9D50DD),
+            icon: null,
+            channelShowBadge: true,
+            importance: NotificationImportance.High,
+            ledColor: Colors.red)
+      ],
+      // Channel groups are only visual and are not required
+      channelGroups: [
+        NotificationChannelGroup(
+            channelGroupkey: 'basic_channel_group',
+            channelGroupName: 'Basic group')
+      ],
+      debug: true);
+
   runApp(const MyApp());
 }
 
@@ -19,7 +44,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  MultiProvider(
+    return MultiProvider(
       providers: [
         Provider<AuthenticationProvider>(
           create: (_) => AuthenticationProvider(FirebaseAuth.instance),
@@ -29,11 +54,12 @@ class MyApp extends StatelessWidget {
           initialData: null,
         ),
       ],
-      child: MaterialApp(
+      child: const MaterialApp(
         debugShowCheckedModeBanner: false,
-        title: 'Material App', 
-        home: Authenticate(returnClass: const Home(),),
-        ),
+        title: 'Material App',
+        home: BuatBadge(),
+        //home: Authenticate(returnClass: const Home(),),
+      ),
     );
   }
 }
